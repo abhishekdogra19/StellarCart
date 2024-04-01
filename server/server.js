2bf659b82277b049e0c8b7fb586671f5c3581522
abhishek dogra <abhidograaa19@gmail.com>
2024-04-01 23:38:28 +0530

    Fix CORS for Build.

--- server/server.js
+++ server/server.js
@@ -2,19 +2,46 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const path = require("path");
 require("dotenv").config();
-app.use(cors());
+const allowedOrigins = [
+  "http://localhost:5173",
+  "http://localhost:4000",
+  "https://stellarcart-dmtu.onrender.com",
+];
+
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (allowedOrigins.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  optionsSuccessStatus: 204,
+  credentials: true, // Allow credentials like cookies
+};
+app.use(cors(corsOptions));
+
 const port = process.env.PORT;
 const Stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
+app.use(express.static(path.join(__dirname, "..", "client", "dist")));
 app.post("/pay", async (req, res) => {
   await Stripe.charges.create({
     source: req.body.token.id,
     amount: req.body.amount,
     currency: "inr",
+  });
+});
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/dist/index.html"), (err) => {
+    if (err) {
+      console.error("Error sending file:", err);
+    }
   });
 });
 app.listen(port, () => {
