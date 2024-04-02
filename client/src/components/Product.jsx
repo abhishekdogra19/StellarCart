ef4190de0a3c3650bb9b80d1e8987031d9e89f74
abhishek dogra <abhidograaa19@gmail.com>
2024-04-02 22:34:09 +0530

    Fixed UI issue in Single Product page.

--- client/src/components/Product.jsx
+++ client/src/components/Product.jsx
@@ -16,7 +16,7 @@ const Product = () => {
   }, []);
   return (
     <div>
-      <div className="max-w-screen-xl mx-auto my-10 flex gap-10">
+      <div className="max-w-screen-xl mx-auto my-10 flex flex-col lg:flex-row p-4 gap-10">
         <div>
           <img
             src={details.image}
@@ -24,7 +24,7 @@ const Product = () => {
             className="w-full h-[550px] object-cover "
           />
         </div>
-        <div className="w-3/5  flex flex-col justify-center gap-12">
+        <div className="lg:w-3/5  flex flex-col justify-center gap-12">
           <div>
             <h2 className="text-4xl font-semibold">{details.title}</h2>
             <div className=" flex items-center gap-4 mt-3">
@@ -45,8 +45,8 @@ const Product = () => {
             <p className="text-xs text-gray-500">(1 Customer Review)</p>
           </div>
           <p className="text-base text-gray-500 -mt-3">{details.description}</p>
-          <div className="flex items-center gap-4">
-            <div className="w-52 flex items-center p-3 justify-between border gap-4 ">
+          <div className="flex flex-col lg:flex-row items-center gap-4">
+            <div className="w-full lg:w-52 flex items-center p-3 justify-between border gap-4 ">
               <p className="text-sm">Quantity</p>
               <div className="flex items-center gap-4 text-sm font-semibold">
                 <button
@@ -79,7 +79,7 @@ const Product = () => {
                   })
                 ) & toast.success(`${details.title} is added`)
               }
-              className="bg-black text-white py-3 px-6 active:bg-gray-800"
+              className="bg-black text-white py-3 px-6 w-full lg:w-52 rounded-lg active:bg-gray-800"
             >
               Add to cart
             </button>
